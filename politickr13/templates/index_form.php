0a21fa0bdd44650321e338fa86cfc4f897fdba5a
heiscody <heiscody@gmail.com>
2014-01-02 02:00:36 -0500

    fixed bug in index_form.php
    Passed mysql id rather than govtrack id with get to display.php

--- politickr13/templates/index_form.php
+++ politickr13/templates/index_form.php
@@ -4,7 +4,7 @@
 		foreach ( $repinformation as $reps)
 		{
 			print("<div class=\"col-md-4\">");
-			print("<a href=\"display.php?id={$reps[0]["id"]}\"><img src=\"{$reps[0]["photourl"]}\"  height=\"200\" width=\"180\"></a><br>");
+			print("<a href=\"display.php?id={$reps[0]["govtrackid"]}\"><img src=\"{$reps[0]["photourl"]}\"  height=\"200\" width=\"180\"></a><br>");
 			print("<a href=\"display.php?id={$reps[0]["id"]}\">{$reps[0]["firstname"]} {$reps[0]["lastname"]}</a><br>");
 			print("{$reps[0]["description"]}<br>");       
 			print("{$reps[0]["party"]}"); 
