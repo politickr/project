7815fb79ead4c429a81dc6e8b89b94110796322c
heiscody <heiscody@gmail.com>
2014-01-01 21:10:21 -0500

    change load test to test load function

--- politickr13/politickr.us/loadtest.php
+++ politickr13/politickr.us/loadtest.php
@@ -3,8 +3,10 @@ require("../includes/config.php");
  
 //$rows = query("SELECT firstname FROM representatives WHERE id = ?", 1);
 //dump($rows);
-$test = new User('name','heiscody@gmail.com', new Legislator(1), new Legislator(2), new Legislator(3), 2);
-dump($test->getName());
+//$test = new User('name','heiscody@gmail.com', new Legislator(1), new Legislator(2), new Legislator(3), 2);
+//dump($test->getName());
+$x=load();
+dump($x);
 //billNews("Test");
 
 //load();
