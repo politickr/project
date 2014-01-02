546b1471e0c7ebab6bacca43d390f770a00e48d1
heiscody <heiscody@gmail.com>
2014-01-02 14:56:39 -0500

    adding date to display_form.php
    Added dump to see the way to navigate vote json

--- politickr13/templates/display_form.php
+++ politickr13/templates/display_form.php
@@ -3,6 +3,7 @@
 <?php
     //prints headers
     print("<tr>");
+	print("<th>Date</th>");
     print("<th>Question</th>");
     print("<th>Ballot</th>");
     print("</tr>");
@@ -13,6 +14,8 @@
     	if( strcmp($vote['vote']['category'], "passage") == 0)
     	{
         	print("<tr>");
+			dump($vote);
+			print("<td>{$vote["option"]["value"]}</td>);
         	print("<td><a href=\"bill.php?bill={$vote["vote"]["related_bill"]}&totalplusbill={$vote["vote"]["total_plus"]}&totalminusbill={$vote["vote"]["total_minus"]}&totalotherbill={$vote["vote"]["total_other"]}\">{$vote["vote"]["question"]}</a></td>");
         	print("<td>{$vote["option"]["value"]}</td>");
         	print("</tr>");
