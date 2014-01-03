7eb642d8f7d1735bbab90ff110ca175fc97e1729
Chris Walter <cwalter94@berkeley.edu>
2014-01-03 15:41:59 -0500

    Deleted all the JPGs. Not sure what's gonna happen...we'll see. Also fixed homepage style.

--- politickr13/templates/homepage_form.php
+++ politickr13/templates/homepage_form.php
@@ -9,57 +9,48 @@
 </script> 
 <div class="container-full">
 
-      
 	<div class="row">
-    	<div class="col-sm-1"></div>
-		<div class="col-sm-4 text-center">
-			<h1 class="main-header-text">Stay informed.</h1>
-		</div>
-        <div class="col-sm-2"></div>
-		<div class="col-sm-4 text-center">
-			<h1 class="main-header-text">Get involved.</h1>
-		</div>
-        <div class="col-sm-1"></div>
-	</div>
-       
-	<br /><br /><br /><br />
-    
-	<div class="row">
-		<div class="col-md-4 text-center v-center">
-        	<div class="col-md-4"></div>
-			<form class="col-md-8" action="search.php" method="post" role="search">
-				<div class="form-group">
-					<input type="text" name="address" class="form-control input-lg" placeholder="Enter your address">
-				</div>
-				<br />
-				<button class="btn btn-lg btn-primary" type="submit">Find Reps</button>
-			</form>
+    	<div class="col-lg-2"></div>
+		<div class="col-lg-3 text-center">
+        	<h1 class="main-header-text">Stay informed.</h1>
+            <br /><br /><br /><br />
+				<form class="col-md-12" action="search.php" method="post" role="search">
+					<div class="form-group">
+						<input type="text" name="address" class="form-control input-lg" placeholder="Enter your address">
+					</div>
+					<button class="btn btn-lg btn-default" type="submit">Find Reps</button>
+				</form>
 		</div>
 
 
-        <div class="col-md-4 text-center">
+        <div class="col-lg-2 text-center">
+        <br /><br />
         	<img src="img/main-or.svg" />
         </div>
         
-        <div class="col-md-4 text-center">
-        	<form class="col-md-8" action="register.php" method="post" role="register">
-         	<br /><br />
-            	<div class="form-group">
-                	<input autofocus class="form-control" name="username" placeholder="Username" type="text"/>
-                </div>
-                <div class="form-group" style="color:blue;">
-            		<input class="form-control" name="email" placeholder="Email" type="Email Address"/>
-        		</div>
-                <div class="form-group">
-                	<input class="form-control" name="address" placeholder="Street Address" type="text"/>
-                </div>
-                <div class="form-group">
-                	<input class="form-control" name="password" placeholder="Password" type="password"/>
-            	</div>
-              		<button class="btn btn-lg" type="submit">Register</button>
-          	</form>
-            <div class="col-md-4"></div>
+        
+        <div class="col-lg-3 text-center">
+        	<h1 class="main-header-text">Get involved.</h1>
+            
+        		<form class="col-md-12 text-center" action="register.php" method="post" role="register">
+         		<br /><br />
+            		<div class="form-group">
+                		<input autofocus class="form-control" name="username" placeholder="Username" type="text"/>
+                	</div>
+                	<div class="form-group" style="color:blue;">
+            			<input class="form-control" name="email" placeholder="Email" type="Email Address"/>
+        			</div>
+                    <div class="form-group">
+                        <input class="form-control" name="address" placeholder="Street Address" type="text"/>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" name="password" placeholder="Password" type="password"/>
+                    </div>
+                        <button class="btn btn-lg btn-default" id="btn-red" type="submit">Register</button>
+                </form>
         </div>
+        <div class="col-lg-2"></div>
+        
   		<br><br><br><br><br>
         
     </div>
@@ -72,31 +63,22 @@
   	<div class="row">
         <div class="col-md-4">
           <div class="panel panel-default">
-            <div class="panel-heading"><h3>Hello.</h3></div>
-            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-            Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-            dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-            Aliquam in felis sit amet augue.
+            <div class="panel-heading"><h3>Learn</h3></div>
+            <div class="panel-body">Keep up-to-date on your elected officials' activities - see their voting records and news appearances, and make sure they're still working for you. 
             </div>
           </div>
         </div>
       	<div class="col-md-4">
         	<div class="panel panel-default">
-            <div class="panel-heading"><h3>Hello.</h3></div>
-            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-            Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-            dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-            Aliquam in felis sit amet augue.
+            <div class="panel-heading"><h3>Follow</h3></div>
+            <div class="panel-body">Make an account and add officials who interest you - their activity will appear on your feed and you can receive email updates if something newsworthy happens. 
             </div>
           </div>
         </div>
       	<div class="col-md-4">
         	<div class="panel panel-default">
-            <div class="panel-heading"><h3>Hello.</h3></div>
-            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-            Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-            dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-            Aliquam in felis sit amet augue.
+            <div class="panel-heading"><h3>Participate</h3></div>
+            <div class="panel-body">Who said the polls are only open in November? You can show support or disapproval for any activity by upvoting or downvoting anything on your feed. Now your representatives know how you feel 24 hours a day, 365 days a year.
             </div>
           </div>
         </div>
