c932d9e5193ccf1e19201f859a52f336e7b758c2
Chris Walter <cwalter94@berkeley.edu>
2014-01-05 04:49:02 -0500

    Got D3.js to work, still playing with the SVG settings.

--- politickr13/templates/display_form.php
+++ politickr13/templates/display_form.php
@@ -28,13 +28,66 @@
 <body>
 <script type="text/javascript">
 	var data = <?php echo $votes ?>;
-	var dataset = data.objects;
-			d3.select("body").selectAll("p")
+	var datatwo = data.objects;
+	dataset = datatwo.slice(0, 300);
+	months = [];
+	months["1"] = "Jan";
+	months["2"] = "Feb";
+	months["3"] = "Mar";
+	months["4"] = "Apr";
+	months["5"] = "May";
+	months["6"] = "Jun";
+	months["7"] = "Jul";
+	months["8"] = "Aug";
+	months["9"] = "Sep";
+	months["10"] = "Oct";
+	months["11"] = "Nov";
+	months["12"] = "Dec";
+	
+	var rectContainers = d3.select("body").selectAll("svg")
 				.data(dataset)
 				.enter()
-				.append("p")
-				.text(function(d) { return d; });
-			
+				.append("svg")
+				.attr("class", "vote")
+				.attr("width", 720)
+				.attr("height", 100)
+				.attr("viewBox", "0 0 720 100")
+				.style("border", "solid")
+				.style("border-radius", "5px");
+				
+	rectContainers.append("rect")
+				.attr("width", 80)
+				.attr("height", 100)
+				.attr("fill", function(d, i) {
+					if (d.option.value == "Yea") {
+						return "#00F100";
+						}
+					return "#F10000";
+					});
+					
+				
+	rectContainers.append("text")
+				.attr("fill", "#FFFFFF")
+				.attr("transform", "matrix(1 0 0 1 16 72.1484)")
+				.text(function(d, i) {
+					var date = d.created;
+					var year = date.substring(0, 4);
+					var month = date.substring(5, 7);
+					var day = date.substring(8, 10);
+					var hour = parseInt(date.substring(11, 13)) % 12;
+					
+					return months[month] + " " + day;
+					});	
+					
+	rectContainers.append("foreignObject")
+				.attr("x", 300) 
+				.attr("y", 20)
+				.attr("width", 400)
+				.attr("height", 80)
+				.text(function(d, i) {
+					var q = d.vote.question;
+					return q;
+					});	
 	</script>
 </body>
 
