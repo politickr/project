a552f8df935c473eedcbdad95c21b601ab65105b
heiscody <heiscody@gmail.com>
2014-01-06 20:14:42 -0500

    Attempt to pass array via get on index_form.php

--- politickr13/politickr.us/my_reps.php
+++ politickr13/politickr.us/my_reps.php
@@ -1,26 +1,8 @@
 <?php
 require("../includes/config.php"); 
 
-if( !empty($_SESSION["user"]["senator1id"]) || !empty($_SESSION["user"]["senator2id"]) || !empty($_SESSION["user"]["repid"]))
-	{
-		 $order = array( 0 => $_SESSION["user"]["senator1id"],
-						1 => $_SESSION["user"]["senator2id"],
-						2 => $_SESSION["user"]["repid"],
-						);
-		 $repinformation = getSavedReps($order);
-		 
-		 if($repinformation === NULL)
-        {
-            apologize("There was an error in retrieving your representatives.");
-        }
-        else
-        {
-            render("index_form.php",  ["repinformation" => $repinformation]);
-        }            
-		
-	}
 	
-	else  if ($_SERVER["REQUEST_METHOD"] == "POST")
+	if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
         // validate submission
         if (empty($_POST["address"]))
@@ -45,6 +27,24 @@ if( !empty($_SESSION["user"]["senator1id"]) || !empty($_SESSION["user"]["senator
             render("index_form.php", ["repinformation" => $repinformation]);
         }            
     }
+	else if( !empty($_SESSION["user"]["senator1id"]) || !empty($_SESSION["user"]["senator2id"]) || !empty($_SESSION["user"]["repid"]))
+	{
+		 $order = array( 0 => $_SESSION["user"]["senator1id"],
+						1 => $_SESSION["user"]["senator2id"],
+						2 => $_SESSION["user"]["repid"],
+						);
+		 $repinformation = getSavedReps($order);
+		 
+		 if($repinformation === NULL)
+        {
+            apologize("There was an error in retrieving your representatives.");
+        }
+        else
+        {
+            render("index_form.php",  ["repinformation" => $repinformation]);
+        }            
+		
+	}
 	else if (!empty($_SESSION["address"]))
 	{
 		 $ids = getReps($_SESSION['address']);
