5a90f99d5c07b675c290b94c992575af11f71a4a
heiscody <heiscody@gmail.com>
2014-01-06 20:22:07 -0500

    can't pass array via get

--- politickr13/templates/index_form.php
+++ politickr13/templates/index_form.php
@@ -15,8 +15,8 @@
 			echo 'repObj['.$i.']='.$reps[0].';';
 			print('</script>');
 			print('<div class="col-lg-4 text-center rep">');
-			print("<a href='display.php?id=".$reps[0]."'><img src=".$reps[0]['photourl']." width='150px'> </a><br>");
-			print("<a href='display.php?id=".$reps[0]."'>".$reps[0]['firstname']." ".$reps[0]['lastname']."</a><br>");
+			print("<a href='display.php?id=".$reps[0]['govtrackid']."'><img src=".$reps[0]['photourl']." width='150px'> </a><br>");
+			print("<a href='display.php?id=".$reps[0]['govtrackid']."'>".$reps[0]['firstname']." ".$reps[0]['lastname']."</a><br>");
 			print("{$reps[0]["description"]}<br>"); 
 			print("{$reps[0]["party"]}");
 			print("</div>");  
