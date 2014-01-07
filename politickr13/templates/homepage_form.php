de81401f6a53ea812ba4c49dee4f6c6aaa2423ea
Chris Walter <cwalter94@berkeley.edu>
2014-01-06 21:37:02 -0500

    Revert 3b99f74..efd159c
    This rolls back to commit 3b99f744cbf6d30ad43ccb7204bd8c1463fa9072.

--- politickr13/templates/homepage_form.php
+++ politickr13/templates/homepage_form.php
@@ -14,7 +14,7 @@
         
 		<div class="col-lg-6 text-center">
         		<div class="something text-center v-center" style="position:relative; min-height:300px; width:40%">
-        					<img id="main0" src="img/main.svg" style="position:absolute;"/>
+        					<img id="main0" src="img/main.svg" style="position:absolute; display:inline-block"/>
                             <form action="search.php" method="post" class="form-signin" id="main1" style="position:absolute; display:none">
         						<input type="text" name="address" class="input-block-level" placeholder="Street Address">
         						<button class="btn btn-large btn-primary" type="submit">Find Representatives</button>
