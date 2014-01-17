b3789e502b7b070f502294949efd344c0cba78af
heiscody <heiscody@gmail.com>
2014-01-16 19:35:48 -0500

    added function readCTCTXT()
    adds fax number and contact form url into representatives table from
    contactingthecongress.org

--- politickr13/politickr.us/loadtest.php
+++ politickr13/politickr.us/loadtest.php
@@ -1,6 +1,7 @@
 <?php
 require("../includes/config.php");
-$temp = array();
-array_push($temp, $_SESSION['allVotes0']);
-dump($temp);
+//$temp = array();
+//array_push($temp, $_SESSION['allVotes0']);
+//dump($temp);
+dump(readCTCTXT());
 ?>
